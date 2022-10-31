5e89bf17ab93ab7ddf6011b567d9bff0007fe98e
Markfryazino <Rofinmp@gmail.com>
2022-10-31 21:51:06 +0300

    initial

---
+++ src/util_classes.py
@@ -0,0 +1,331 @@
+from typing import Tuple, Union, List, Callable, Optional
+from tqdm import tqdm
+from itertools import islice
+import pathlib
+
+import numpy as np
+import pandas as pd
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch import distributions
+from torch.utils.data import DataLoader, Dataset, WeightedRandomSampler
+from torch.nn.utils.rnn import pad_sequence
+
+import torchaudio
+from IPython import display as display_
+from collections import defaultdict
+from IPython.display import clear_output
+from matplotlib import pyplot as plt
+
+
+
+class SpeechCommandDataset(Dataset):
+
+    def __init__(
+        self,
+        transform: Optional[Callable] = None,
+        path2dir: str = None,
+        keywords: Union[str, List[str]] = None,
+        csv: Optional[pd.DataFrame] = None
+    ):        
+        self.transform = transform
+
+        if csv is None:
+            path2dir = pathlib.Path(path2dir)
+            keywords = keywords if isinstance(keywords, list) else [keywords]
+            
+            all_keywords = [
+                p.stem for p in path2dir.glob('*')
+                if p.is_dir() and not p.stem.startswith('_')
+            ]
+
+            triplets = []
+            for keyword in all_keywords:
+                paths = (path2dir / keyword).rglob('*.wav')
+                if keyword in keywords:
+                    for path2wav in paths:
+                        triplets.append((path2wav.as_posix(), keyword, 1))
+                else:
+                    for path2wav in paths:
+                        triplets.append((path2wav.as_posix(), keyword, 0))
+            
+            self.csv = pd.DataFrame(
+                triplets,
+                columns=['path', 'keyword', 'label']
+            )
+
+        else:
+            self.csv = csv
+    
+    def __getitem__(self, index: int):
+        instance = self.csv.iloc[index]
+
+        path2wav = instance['path']
+        wav, sr = torchaudio.load(path2wav)
+        wav = wav.sum(dim=0)
+        
+        if self.transform:
+            wav = self.transform(wav)
+
+        return {
+            'wav': wav,
+            'keywors': instance['keyword'],
+            'label': instance['label']
+        }
+
+    def __len__(self):
+        return len(self.csv)
+
+
+class AugsCreation:
+
+    def __init__(self):
+        self.background_noises = [
+            'speech_commands/_background_noise_/white_noise.wav',
+            'speech_commands/_background_noise_/dude_miaowing.wav',
+            'speech_commands/_background_noise_/doing_the_dishes.wav',
+            'speech_commands/_background_noise_/exercise_bike.wav',
+            'speech_commands/_background_noise_/pink_noise.wav',
+            'speech_commands/_background_noise_/running_tap.wav'
+        ]
+
+        self.noises = [
+            torchaudio.load(p)[0].squeeze()
+            for p in self.background_noises
+        ]
+
+    def add_rand_noise(self, audio):
+
+        # randomly choose noise
+        noise_num = torch.randint(low=0, high=len(
+            self.background_noises), size=(1,)).item()
+        noise = self.noises[noise_num]
+
+        noise_level = torch.Tensor([1])  # [0, 40]
+
+        noise_energy = torch.norm(noise)
+        audio_energy = torch.norm(audio)
+        alpha = (audio_energy / noise_energy) * \
+            torch.pow(10, -noise_level / 20)
+
+        start = torch.randint(
+            low=0,
+            high=max(int(noise.size(0) - audio.size(0) - 1), 1),
+            size=(1,)
+        ).item()
+        noise_sample = noise[start: start + audio.size(0)]
+
+        audio_new = audio + alpha * noise_sample
+        audio_new.clamp_(-1, 1)
+        return audio_new
+
+    def __call__(self, wav):
+        aug_num = torch.randint(low=0, high=4, size=(1,)).item()   # choose 1 random aug from augs
+        augs = [
+            lambda x: x,
+            lambda x: (x + distributions.Normal(0, 0.01).sample(x.size())).clamp_(-1, 1),
+            lambda x: torchaudio.transforms.Vol(.25)(x),
+            lambda x: self.add_rand_noise(x)
+        ]
+
+        return augs[aug_num](wav)
+
+
+def get_sampler(target):
+    class_sample_count = np.array(
+        [len(np.where(target == t)[0]) for t in np.unique(target)])   # for every class count it's number of occ.
+    weight = 1. / class_sample_count
+    samples_weight = np.array([weight[t] for t in target])
+    samples_weight = torch.from_numpy(samples_weight)
+    samples_weigth = samples_weight.float()
+    sampler = WeightedRandomSampler(samples_weight, len(samples_weight))
+    return sampler
+
+
+class Collator:
+    def __call__(self, data):
+        wavs = []
+        labels = []    
+
+        for el in data:
+            wavs.append(el['wav'])
+            labels.append(el['label'])
+
+        # torch.nn.utils.rnn.pad_sequence takes list(Tensors) and returns padded (with 0.0) Tensor
+        wavs = pad_sequence(wavs, batch_first=True)    
+        labels = torch.Tensor(labels).long()
+        return wavs, labels
+
+
+def get_train_test_dataloaders(path2dir, config):
+    dataset = SpeechCommandDataset(
+        path2dir=path2dir, keywords=config.keyword
+    )
+
+    indexes = torch.randperm(len(dataset))
+    train_indexes = indexes[:int(len(dataset) * 0.8)]
+    val_indexes = indexes[int(len(dataset) * 0.8):]
+
+    train_df = dataset.csv.iloc[train_indexes].reset_index(drop=True)
+    val_df = dataset.csv.iloc[val_indexes].reset_index(drop=True)
+
+    # Sample is a dict of utt, word and label
+    train_set = SpeechCommandDataset(csv=train_df, transform=AugsCreation())
+    val_set = SpeechCommandDataset(csv=val_df)
+
+    train_sampler = get_sampler(train_set.csv['label'].values)
+
+    train_loader = DataLoader(train_set, batch_size=config.batch_size,
+                            shuffle=False, collate_fn=Collator(),
+                            sampler=train_sampler,
+                            num_workers=2, pin_memory=True)
+
+    val_loader = DataLoader(val_set, batch_size=config.batch_size,
+                            shuffle=False, collate_fn=Collator(),
+                            num_workers=2, pin_memory=True)
+
+    return train_loader, val_loader
+
+
+class LogMelspec:
+
+    def __init__(self, is_train, config):
+        # with augmentations
+        if is_train:
+            self.melspec = nn.Sequential(
+                torchaudio.transforms.MelSpectrogram(
+                    sample_rate=config.sample_rate,
+                    n_fft=400,
+                    win_length=400,
+                    hop_length=160,
+                    n_mels=config.n_mels
+                ),
+                torchaudio.transforms.FrequencyMasking(freq_mask_param=15),
+                torchaudio.transforms.TimeMasking(time_mask_param=35),
+            ).to(config.device)
+
+        # no augmentations
+        else:
+            self.melspec = torchaudio.transforms.MelSpectrogram(
+                sample_rate=config.sample_rate,
+                n_fft=400,
+                win_length=400,
+                hop_length=160,
+                n_mels=config.n_mels
+            ).to(config.device)
+
+    def __call__(self, batch):
+        # already on device
+        return torch.log(self.melspec(batch).clamp_(min=1e-9, max=1e9))
+
+
+def count_FA_FR(preds, labels):
+    FA = torch.sum(preds[labels == 0])
+    FR = torch.sum(labels[preds == 0])
+    
+    # torch.numel - returns total number of elements in tensor
+    return FA.item() / torch.numel(preds), FR.item() / torch.numel(preds)
+
+
+def get_au_fa_fr(probs, labels):
+    sorted_probs, _ = torch.sort(probs)
+    sorted_probs = torch.cat((torch.Tensor([0]), sorted_probs, torch.Tensor([1])))
+    labels = torch.cat(labels, dim=0)
+        
+    FAs, FRs = [], []
+    for prob in sorted_probs:
+        preds = (probs >= prob) * 1
+        FA, FR = count_FA_FR(preds, labels)        
+        FAs.append(FA)
+        FRs.append(FR)
+    # plt.plot(FAs, FRs)
+    # plt.show()
+
+    # ~ area under curve using trapezoidal rule
+    return -np.trapz(FRs, x=FAs)
+
+
+def train_epoch(model, opt, loader, log_melspec, device):
+    model.train()
+    for i, (batch, labels) in tqdm(enumerate(loader), total=len(loader)):
+        batch, labels = batch.to(device), labels.to(device)
+        batch = log_melspec(batch)
+
+        opt.zero_grad()
+
+        # run model # with autocast():
+        logits = model(batch)
+        # we need probabilities so we use softmax & CE separately
+        probs = F.softmax(logits, dim=-1)
+        loss = F.cross_entropy(logits, labels)
+
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 5)
+
+        opt.step()
+
+        # logging
+        argmax_probs = torch.argmax(probs, dim=-1)
+        FA, FR = count_FA_FR(argmax_probs, labels)
+        acc = torch.sum(argmax_probs == labels) / torch.numel(argmax_probs)
+
+    return acc
+
+
+@torch.no_grad()
+def validation(model, loader, log_melspec, device):
+    model.eval()
+
+    val_losses, accs, FAs, FRs = [], [], [], []
+    all_probs, all_labels = [], []
+    for i, (batch, labels) in tqdm(enumerate(loader)):
+        batch, labels = batch.to(device), labels.to(device)
+        batch = log_melspec(batch)
+
+        output = model(batch)
+        # we need probabilities so we use softmax & CE separately
+        probs = F.softmax(output, dim=-1)
+        loss = F.cross_entropy(output, labels)
+
+        # logging
+        argmax_probs = torch.argmax(probs, dim=-1)
+        all_probs.append(probs[:, 1].cpu())
+        all_labels.append(labels.cpu())
+        val_losses.append(loss.item())
+        accs.append(
+            torch.sum(argmax_probs == labels).item() /  # ???
+            torch.numel(argmax_probs)
+        )
+        FA, FR = count_FA_FR(argmax_probs, labels)
+        FAs.append(FA)
+        FRs.append(FR)
+
+    # area under FA/FR curve for whole loader
+    au_fa_fr = get_au_fa_fr(torch.cat(all_probs, dim=0).cpu(), all_labels)
+    return au_fa_fr
+
+
+def train(model, optimizer, train_loader, val_loader, melspec_train, melspec_val, config):
+    history = defaultdict(list)
+
+    for n in range(config.num_epochs):
+
+        train_epoch(model, optimizer, train_loader,
+                    melspec_train, config.device)
+
+        au_fa_fr = validation(model, val_loader,
+                            melspec_val, config.device)
+        history['val_metric'].append(au_fa_fr)
+
+        clear_output()
+        plt.plot(history['val_metric'])
+        plt.ylabel('Metric')
+        plt.xlabel('Epoch')
+        plt.grid()
+        plt.show()
+
+        print('END OF EPOCH', n)
+    
+    return model, history
